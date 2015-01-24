5952723f8a41b0de4a738f543b8e5707950c3ed5
michael bunch <michael8818bunch@gmail.com>
2015-01-24 07:45:35 -0500

    Intro Networking

---
+++ GGJ15/Assets/Scripts/Find Opponent/FindOpponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindOpponent : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("FIND OPPONENT");
+		if(PhotonNetwork.otherPlayers.Length > 0){
+			PhotonNetwork.JoinOrCreateRoom(PhotonNetwork.playerName,new RoomOptions(){maxPlayers = 2},null);
+			photonView.RPC("FoundOpponent",PhotonNetwork.otherPlayers[0]);
+		}
+		else{
+			Debug.Log("No other players");
+		}
+	}
+
+	public void OnJoinedRoom()
+	{
+		Debug.Log("OnJoinedRoom: " + PhotonNetwork.player.isMasterClient);
+	}
+
+	public void OnCreatedRoom()
+	{
+		Debug.Log("OnCreatedRoom");
+	}
+
+	[RPC]
+	public void FoundOpponent(PhotonMessageInfo info){
+		PhotonNetwork.JoinOrCreateRoom(info.sender.name,new RoomOptions(){maxPlayers = 2},null);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GGJ15/Assets/Scripts/Start Screen/StartScreen.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class StartScreen : MonoBehaviour {
+
+	public GUISkin Skin;
+	public Vector2 WidthAndHeight = new Vector2(600,400);
+	private string roomName = "FindOpponent";
+	
+	private bool connectFailed = false;
+	
+	public static readonly string SceneNameGame = "FindOpponent";
+	
+	public void Awake()
+	{
+		// this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+		PhotonNetwork.automaticallySyncScene = true;
+		
+		// the following line checks if this client was just created (and not yet online). if so, we connect
+		if (PhotonNetwork.connectionStateDetailed == PeerState.PeerCreated)
+		{
+			// Connect to the photon master-server. We use the settings saved in PhotonServerSettings (a .asset file in this project)
+			PhotonNetwork.ConnectUsingSettings("0.9");
+		}
+		
+		// generate a name for this player, if none is assigned yet
+		if (String.IsNullOrEmpty(PhotonNetwork.playerName))
+		{
+			PhotonNetwork.playerName = "Guest" + Random.Range(1, 9999);
+		}
+		
+		// if you wanted more debug out, turn this on:
+		// PhotonNetwork.logLevel = NetworkLogLevel.Full;
+	}
+	
+	public void OnGUI()
+	{
+		if (this.Skin != null)
+		{
+			GUI.skin = this.Skin;
+		}
+		
+		if (!PhotonNetwork.connected)
+		{
+			if (PhotonNetwork.connecting)
+			{
+				GUILayout.Label("Connecting to: " + PhotonNetwork.ServerAddress);
+			}
+			else
+			{
+				GUILayout.Label("Not connected. Check console output. Detailed connection state: " + PhotonNetwork.connectionStateDetailed + " Server: " + PhotonNetwork.ServerAddress);
+			}
+			
+			if (this.connectFailed)
+			{
+				GUILayout.Label("Connection failed. Check setup and use Setup Wizard to fix configuration.");
+				GUILayout.Label(String.Format("Server: {0}", new object[] {PhotonNetwork.ServerAddress}));
+				GUILayout.Label("AppId: " + PhotonNetwork.PhotonServerSettings.AppID);
+				
+				if (GUILayout.Button("Try Again", GUILayout.Width(100)))
+				{
+					this.connectFailed = false;
+					PhotonNetwork.ConnectUsingSettings("0.9");
+				}
+			}
+			
+			return;
+		}
+		
+		Rect content = new Rect((Screen.width - WidthAndHeight.x)/2, (Screen.height - WidthAndHeight.y)/2, WidthAndHeight.x, WidthAndHeight.y);
+		GUI.Box(content,"Party Foul");
+		GUILayout.BeginArea(content);
+		
+		GUILayout.Space(40);
+		
+		// Player name
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Player name:", GUILayout.Width(150));
+		PhotonNetwork.playerName = GUILayout.TextField(PhotonNetwork.playerName);
+		GUILayout.Space(158);
+		if (GUI.changed)
+		{
+			// Save name
+			PlayerPrefs.SetString("playerName", PhotonNetwork.playerName);
+		}
+		GUILayout.EndHorizontal();
+		
+		GUILayout.Space(15);
+		
+		// Join room by title
+		GUILayout.BeginHorizontal();
+		
+		if (GUILayout.Button("Find Opponent", GUILayout.Width(150)))
+		{
+			PhotonNetwork.JoinOrCreateRoom(this.roomName, new RoomOptions() { maxPlayers = 20 }, null);
+		}
+		
+		GUILayout.EndHorizontal();
+		
+		GUILayout.EndArea();
+	}
+	
+	// We have two options here: we either joined(by title, list or random) or created a room.
+	public void OnJoinedRoom()
+	{
+		Debug.Log("OnJoinedRoom");
+	}
+	
+	
+	public void OnPhotonCreateRoomFailed()
+	{
+		Debug.Log("OnPhotonCreateRoomFailed got called. This can happen if the room exists (even if not visible). Try another room name.");
+	}
+	
+	public void OnPhotonJoinRoomFailed()
+	{
+		Debug.Log("OnPhotonJoinRoomFailed got called. This can happen if the room is not existing or full or closed.");
+	}
+	public void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("OnPhotonRandomJoinFailed got called. Happens if no room is available (or all full or invisible or closed). JoinrRandom filter-options can limit available rooms.");
+	}
+	
+	public void OnCreatedRoom()
+	{
+		Debug.Log("OnCreatedRoom");
+		PhotonNetwork.LoadLevel(SceneNameGame);
+	}
+	
+	public void OnDisconnectedFromPhoton()
+	{
+		Debug.Log("Disconnected from Photon.");
+	}
+	
+	public void OnFailedToConnectToPhoton(object parameters)
+	{
+		this.connectFailed = true;
+		Debug.Log("OnFailedToConnectToPhoton. StatusCode: " + parameters + " ServerAddress: " + PhotonNetwork.networkingPeer.ServerAddress);
+	}
+}
