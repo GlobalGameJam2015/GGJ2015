1ad035b2ba0d11d2aec49ba10bd9a320af579b59
John Pennington <atmospherium@gmail.com>
2015-01-25 04:05:36 -0500

    FAT GUY IS HERE BUT NOT FUNCTIONAL. HE IS SO FAT. HE IS SO GREAT.

---
+++ GGJ15/Assets/Scripts/Game/Cards/FatGuy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Person))]
+public class Jock : MonoBehaviour {
+	private GameManager Manager;
+	private Person person;
+
+	void Start(){
+		Manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		person = this.GetComponent<Person>();
+	}
+
+	void Played () {
+		//MOVE TO OTHER PLAYER
+
+		//LOSS OF RESOURCES
+		
+		person.SetResourceNeed(3);
+		//+1 Resource for all womens
+	}
+
+	void Moved(){
+		//
+	}
+}
