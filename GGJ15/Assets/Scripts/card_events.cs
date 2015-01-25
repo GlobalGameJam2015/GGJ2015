24becb0948f7d44afcc0e3fc9ef55f74f45baca0
Matt Leonard <knaple.kbshorror.com>
2015-01-24 21:50:06 -0800

    text placeholders

--- GGJ15/Assets/Scripts/card_events.cs
+++ GGJ15/Assets/Scripts/card_events.cs
@@ -4,30 +4,60 @@ using System.Collections.Generic;
 
 public class card_events : MonoBehaviour {
 	public Vector3 getCurrent;
-	public bool draggable;
 	public bool dragging;
-	//Vector3 screenPos;
-	//public List<string> cardList = new List<string>();
-	
+	public GameObject[] listObjects;
+
 
 	void OnMouseEnter() {
+		int pos = gameObject.name.LastIndexOf("d") + 1;
+		//cardExpandHandle(gameObject.name.Substring(pos, gameObject.name.Length - pos));
+		//Debug.Log (gameObject.name.LastIndexOf("d"));
 		getCurrent = gameObject.transform.position;
 		iTween.MoveTo (gameObject, new Vector3(getCurrent.x, -7.5f, 1), 0.5f);
 		iTween.ScaleTo (gameObject, new Vector3(1.5f,1.5f,1f), 1);
-		draggable = true;
 	}
+	/*
+	void cardExpandHandle(string cardNum) {
+		listObjects = GameObject.FindGameObjectsWithTag ("playerCard");
+		foreach (Object obj in listObjects) {
+			int newpos = obj.name.LastIndexOf("d") + 1;
+			string num = obj.name.Substring(newpos, gameObject.name.Length - newpos);// prints "world"
+			//Debug.Log (obj.name);
+			if (num != cardNum) {
+				Debug.Log (obj.name);
+			}
 
-	void OnMouseOver() {
-		//Debug.Log (this.tag);
+		}
+		Debug.Log (testint);
+		switch (cardNum) {
+		case "1":
+			//Debug.Log(1);
+			break;
+		case "2":
+			//Debug.Log(2);
+			break;
+		case "3":
+			//Debug.Log(3);
+			break;
+		case "4":
+			//Debug.Log(4);
+			break;
+		case "5":
+			//Debug.Log(5);
+			break;
+		default:
+			//Debug.Log ("none");
+			break;
+		}
 
 	}
+*/
 
 	void OnMouseExit() {
 		makeSmall ();
-		draggable = false;
 	}
 	
-	void OnMouseDown() {
+	void OnMouseDownAsButton() {
 		Debug.Log ("clicked");
 	}
 	
