220dc39abf73f076a88a733fc99b1f2392691ecc
Matt Leonard <knaple.kbshorror.com>
2015-01-28 22:42:10 -0800

    Login Screen / Registration Page start. (parse added to project)

---
+++ GGJ15/Assets/Scripts/Login Screen/Parse_Init.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class Parse_Init : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void register_user(string user, string pass, string email) {
+		ParseUser newuser = new ParseUser(){
+			Username = user,
+			Password = pass,
+			Email = email
+		};
+		
+		newuser.SignUpAsync();
+		Debug.Log ("new user " + user + " added");
+	}
+
+	
+}
